b2ae34ea28868d301cd1c5bfcf9474179c8390e9
J <anabih@inmobly.com>
2018-04-14 20:53:28 +0200

    a

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <form method="POST" action="{{route('sections.store')}}">
+        {{csrf_field()}}
+        <input class="form-control col-md-3" placeholder="name" name="name">
+        <input type="color" class=" col-md-3" name="color" placeholder="note">
+        <input class="btn btn-primary" type="submit" value="save">
+    </form>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
 @section('content')
     name / price / color / model / image
+    <a class="btn btn-primary" href="{{route('products.create')}}">create</a>
 @endsection
